db20355897181705591e525160a0d728bb22be75
ekalcrench <hhaekalazmi@gmail.com>
2022-08-14 08:39:16 +0700

    Tambahin Template Email.html

--- src/app/App.js
+++ src/app/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter } from "react-router-dom";
 import { SideMenuComponent } from "../components/side-menu";
 import routes from "../routes";
 import { NavBarComponent } from "../components/nav-bar";
+import { Email } from "../helper/email";
 
 function App() {
   return (
     <div className="app-container">
       <BrowserRouter>
+        <Email />
         <NavBarComponent />
         <div className="app-container-down">
           <SideMenuComponent />
